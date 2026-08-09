c732a36fdf516eb1b9fed49b399e38f9709ca030
Telvin-Muriuki-Mugambi <mugambi.telvin.muriuki@gmail.com>
2026-08-09 22:44:51 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//This is where the routing will be defined. Use of react router is adviced.
+//I have installed react router and material UI for UI elements and components to the repository
+//To avoid merge conflicts, do not edit this page. Leave it for the scrum master.
+//I have a sample DB with products run with npm run server
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
