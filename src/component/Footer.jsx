c05fc384007f433d18168eebc3c5e47fb34dde06
Telvin-Muriuki-Mugambi <mugambi.telvin.muriuki@gmail.com>
2026-08-16 10:13:19 +0300

    Building the footer section. Created the structure

---
+++ src/component/Footer.jsx
@@ -0,0 +1,29 @@
+export default function Footer(){
+    return (
+        <footer>
+            <div>
+                <h3>Legal & Regulatory Disclaimers</h3>
+                <p>The webapp provides information and is not a substitute for professional medical advice, diagnosis, or treatment.</p>
+                <p>HIPAA-compliant Privacy Policy and Terms of Service.</p>
+                <p>Pharmacy and Poisons Board for Kenya-based operations</p>
+            </div>
+
+            <div>
+                <h3>Customer Reach</h3>
+                <p>Nairobi, Kenya</p>
+                <p>info@kappaalphawellness.com</p>
+                <p>+254 0000 000</p>
+            </div>
+
+            <div>
+                <h3>Socials</h3>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                    <li>Tiktok</li>
+                    <li>X</li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
