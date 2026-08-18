7e332b68de8302759ef025134174654e6f0910ce
Telvin-Muriuki-Mugambi <mugambi.telvin.muriuki@gmail.com>
2026-08-18 09:17:30 +0300

    Managed to implement the toast message and add better styling to it

--- src/component/Toast.jsx
+++ src/component/Toast.jsx
@@ -1,19 +1,16 @@
 import { useEffect } from "react";
+import "../styles/toast.css";
 
 export default function Toast({message, duration, onClose}){
     useEffect(() => {
         const timer = setTimeout(() => {
             onClose();
-        }, duration, 3000)
+        }, duration || 3000)
         return () => clearTimeout(timer)
     }, [duration, onClose]);
 
     return (
-        <div style={{
-            position: 'fixed', bottom: '20px', right: '20px',
-            backgroundColor: '#333', color: '#fff', padding: '12px 24px',
-            borderRadius: '4px', boxShadow: '0 2px 10px rgba(0,0,0,0.2)'
-        }}>
+        <div className="toast-message toast-success">
             {message}
         </div>
     )
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import useCart from "../hooks/AddToCart";
+import Toast from "../component/Toast";
 
 export default function Checkout(){
     const {checkedItems, totalPrice} = useCart();
+    const [showToast, setShowToast] = useState(false);
     console.log(checkedItems.length)
     const navigate = useNavigate();
     const [formData, setFormData] = useState({
@@ -23,14 +25,14 @@ export default function Checkout(){
     function handleCheckout(event){
         event.preventDefault();
         console.log(`The user ${formData.firstName} has made an order to the delivery location ${formData.address}`);
-        navigate('/products')
+
     }
     return (
         <section className="admin-page form-section">
 
             <h2 className="section-title">Checkout Page</h2>
             <div className="admin-form">
-               
+               {/* Add the items that have been shopped */}
             </div>
             <form onSubmit={handleCheckout} className="admin-form">
             
@@ -82,10 +84,19 @@ export default function Checkout(){
                 />
                 </div>
 
-                <button type="submit" className="submit-btn">
+                <button type="submit" className="submit-btn" onClick={() => setShowToast(true)}>
                 Order
                 </button>
             </form>
+            {showToast && (
+                <div style={{maxWidth: '560px', margin: '1rem auto'}}>
+                    <Toast
+                        message={"Order has been placed successfully. Thank you for choosing Kappa Alpha Wellness Store"}
+                        duration={3000}
+                        onClose={() => {setShowToast(false); navigate('/products')} }
+                    />
+                </div>
+            )}
     </section>
     )
 }
